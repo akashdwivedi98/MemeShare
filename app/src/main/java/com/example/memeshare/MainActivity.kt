d0c45750b094b225fecf285545e9ed1d138c648c
Akash <akashdwivedi02@gmail.com>
2021-01-28 19:13:09 +0530

    MemeShare App

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.memeshare
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,7 @@ import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var currentImageUrl: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,8 +37,8 @@ class MainActivity : AppCompatActivity() {
           val JsonObjectRequest = JsonObjectRequest(
               Request.Method.GET, url, null,
               Response.Listener { response ->
-                val url = response.getString("url")
-                  Glide.with(this).load(url).listener(object : RequestListener<Drawable> {
+                  currentImageUrl = response.getString("url")
+                  Glide.with(this).load(currentImageUrl).listener(object : RequestListener<Drawable> {
 
                       override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
                          progressBar.visibility = View.GONE
@@ -61,6 +63,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun shareMeme(view: View) {
-
+val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT,"Hey, checkout this cool meme from Reddit $currentImageUrl")
+        val chooser = Intent.createChooser(intent, "Share this meme using....")
+        startActivity(chooser)
     }
 }
